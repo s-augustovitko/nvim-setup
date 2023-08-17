363479d0da97ef569686763983ec87ad7e2f3989
s-augustovitko <salvador.vitko@pluto.tv>
2023-08-17 13:26:56 -0500

    remove crystal

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -31,7 +31,6 @@ return require('packer').startup(function(use)
   use 'romgrk/barbar.nvim'      -- tabs
   use 'itmecho/neoterm.nvim'    -- floating terminal
   use 'm4xshen/autoclose.nvim'  -- autoclose
-  use 'vim-crystal/vim-crystal' -- crystal  support
 
   if packer_bootstrap then
     require('packer').sync()
